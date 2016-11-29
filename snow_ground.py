075a5429f3510b8aa4462a22b0364ed9ca6ad5bc
Sebastian Mischke <SebastianMischke@gmx.de>
2016-11-29 21:03:19 +0100

    Initial commit to create the project

---
+++ snow_ground.py
@@ -0,0 +1,178 @@
+# -*- coding: cp1252 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.collections import PolyCollection
+import math
+import os
+
+
+def create_dir(foldername): # create folder if necessary
+    if not os.path.exists(foldername): # check if folder does not exist
+        os.makedirs(foldername) # creates new folder
+    return foldername # returns the name of the folder
+
+
+def smooth(array, smoothness):
+    for x in range(0, len(array)): # For every x-position in array
+        for y in range(0, len(array[0])): # For every y-position in array
+
+            # If Corners
+            if x==0 and y==0:
+                array[x,y] = (1-smoothness/2) * array[x,y] + (smoothness/4) * array[x+1,y] + (smoothness/4) * array[x,y+1]
+            elif x==0 and y==len(array[0])-1:
+                array[x,y] = (1-smoothness/2) * array[x,y] + (smoothness/4) * array[x+1,y] + (smoothness/4) * array[x,y-1]
+            elif x==len(array)-1 and y==0:
+                array[x,y] = (1-smoothness/2) * array[x,y] + (smoothness/4) * array[x-1,y] + (smoothness/4) * array[x,y+1]
+            elif x==len(array)-1 and y==len(array[0])-1:
+                array[x,y] = (1-smoothness/2) * array[x,y] + (smoothness/4) * array[x-1,y] + (smoothness/4) * array[x,y-1]
+
+            # If Edges
+            elif x==0:
+                array[x,y] = (1-smoothness*3/4) * array[x,y] + (smoothness/4) * array[x+1,y] + (smoothness/4) * array[x,y+1] + (smoothness/4) * array[x,y-1]
+            elif x==len(array)-1:
+                array[x,y] = (1-smoothness*3/4) * array[x,y] + (smoothness/4) * array[x-1,y] + (smoothness/4) * array[x,y+1] + (smoothness/4) * array[x,y-1]
+            elif y==0:
+                array[x,y] = (1-smoothness*3/4) * array[x,y] + (smoothness/4) * array[x+1,y] + (smoothness/4) * array[x-1,y] + (smoothness/4) * array[x,y+1]
+            elif y==len(array[0])-1:
+                array[x,y] = (1-smoothness*3/4) * array[x,y] + (smoothness/4) * array[x+1,y] + (smoothness/4) * array[x-1,y] + (smoothness/4) * array[x,y-1]
+
+            # Remainings
+            else:
+                array[x,y] = (1-smoothness) * array[x,y] + (smoothness/4) * array[x+1,y] + (smoothness/4) * array[x-1,y] + (smoothness/4) * array[x,y+1] + (smoothness/4) * array[x,y-1]
+
+    return array
+
+
+def show_surface(array, snow, filename=None):
+    # Create x and y grid
+    x = np.arange(0, len(array[0]), 1)
+    y = np.arange(0, len(array), 1)
+    xs, ys = np.meshgrid(x, y)
+
+    fig = plt.figure()
+
+    DPI = fig.get_dpi()
+    fig.set_size_inches(1600.0/float(DPI),1200.0/float(DPI))
+
+    ax = Axes3D(fig)
+    ax.set_xlim3d(0, 50) # Set min and max of x axis
+    ax.set_ylim3d(0, 50) # Set min and max of y axis
+    ax.set_zlim3d(0, 1) # Set min and max of z axis
+    ax.plot_surface(xs, ys, array, rstride=1, cstride=1, cmap='GnBu', linewidth=0, antialiased=True)
+
+    # x axis edge
+    values = [0.]
+    points = [0.]
+    for ix in np.arange(0, width-0.999, 0.1):
+        low = int(math.floor(ix))
+        high = int(math.ceil(ix))
+        p = float(ix - low)
+        value = (1. - p) * array[0, low] + p * array[0, high]
+        values.extend([value])
+        points.extend([ix])
+    values.extend([0])
+    points.extend([width-1])
+    poly = PolyCollection([list(zip(points, values))], facecolors = 'b')
+    ax.add_collection3d(poly, zs=0, zdir='y')
+
+    # y axis edge
+    values = [0.]
+    points = [0.]
+    for iy in np.arange(0, height-0.999, 0.1):
+        low = int(math.floor(iy))
+        high = int(math.ceil(iy))
+        p = float(iy - low)
+        value = (1. - p) * array[low, height-1] + p * array[high, height-1]
+        values.extend([value])
+        points.extend([iy])
+    values.extend([0])
+    points.extend([height-1])
+    poly = PolyCollection([list(zip(points, values))], facecolors = ['b'])
+    ax.add_collection3d(poly, zs=height-1, zdir='x')
+
+    # Draw Snowflakes
+    xs = []
+    ys = []
+    zs = []
+    for ix in range(0, height):
+        for iy in range(0, width):
+            if snow[ix, iy] > array[ix, iy]:
+                xs.extend([ix])
+                ys.extend([iy])
+                zs.extend([snow[ix, iy]])
+    ax.scatter(xs, ys, zs, c='r', marker='o', alpha=0.5)
+
+    # Save or show graphic
+    if filename:
+        plt.savefig(filename) # Save the figure
+    else:
+        plt.show() # Show the figure
+    plt.close(fig) # Close the figure
+
+
+def evaluate_snow(snow, array, step=0.05, p=1.0): # Evaluates all snowflakes
+    for ix in range(0, height): # For all x-positions in array
+        for iy in range(0, width): # For all y-positions in array
+            if snow[ix, iy] == 0: # Inactive snowflake
+                if p > np.random.rand(): # Random chance of p
+                    snow[ix, iy] = 1 # Make snowflake active
+            else: # Active snowflake
+                snow[ix, iy] -= np.random.rand() * step # Let snowflakes fall
+                if snow[ix, iy] < array[ix, iy]: # Snowflake lower than snow
+                    snow[ix, iy] = 0 # Make snowflake inactive
+                    array[ix, iy] += 0.025 # Increase snow height at position
+
+
+def save_as_obj(array, filename):
+    f = open(filename, 'w')
+    for x in range(0, len(array)):
+        for y in range(0, len(array[0])):
+            f.write(get_vertex_string(array, x, y))
+    f.write(get_triangles_string(len(array), len(array[0])))
+    f.close() # you can omit in most cases as the destructor will call it
+
+
+def get_vertex_string(array, x, y):
+    return 'v ' + str(x) + ' ' + str(y) + ' ' + str(array[x, y]) + '\n'
+
+
+def get_triangles_string(x_max, y_max):
+
+    number_array = np.zeros((x_max, y_max))
+    print number_array.shape
+    for x in range(0, x_max):
+        for y in range(0, y_max):
+            number_array[x, y] = x * y_max + y + 1
+
+    result = ""
+    for x in range(0, x_max-1):
+        for y in range(0, y_max-1):
+            result += 'f ' + str(number_array[x, y]) + ' ' + str(number_array[x, y+1]) + ' ' + str(number_array[x+1, y]) + '\n'
+            result += 'f ' + str(number_array[x+1, y+1]) + ' ' + str(number_array[x+1, y]) + ' ' + str(number_array[x, y+1]) + '\n'
+    return result
+
+
+def print_array(array):
+    for i in range(0, len(array)):
+        print array[i]
+
+
+array = np.zeros((50, 50)) # Create empty array
+height = len(array) # Height of ground-array (should be 50)
+width = len(array[0]) # Width of ground-array (should be 50)
+snow = np.random.rand(height, width) # Create randomized array
+savedir = create_dir('animation') # Create a folder
+
+
+counter = 0 # Set counter
+while np.average(array) < 1: # While snow height is lower than 1
+
+    evaluate_snow(snow, array, 0.05, 0.1) # Evaluate snowflakes
+
+    smooth(array, 0.1) # Smooth the snow
+
+    #show_surface(array, snow, savedir + "/" + str(i).zfill(4) + '.png') # Save to file
+    show_surface(array, snow) # No save, just show
+
+    counter += 1 # Increase counter
