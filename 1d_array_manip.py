b8f31f66b07c23faf8ade0610f543ab9cc6f1431
Sebastian Mischke <SebastianMischke@gmx.de>
2017-01-25 19:40:13 +0100

    Add show_surface from other file

--- 1d_array_manip.py
+++ 1d_array_manip.py
@@ -39,3 +39,29 @@ for i in range(0, width*height):
 # # Rechts
 
 # Mitte
+def show_surface(paramarray, filename=None):
+
+    array = paramarray.reshape()
+    
+    # Create x and y grid
+    x = np.arange(0, len(array[0]), 1)
+    y = np.arange(0, len(array), 1)
+    xs, ys = np.meshgrid(x, y)
+
+    fig = plt.figure()
+
+    DPI = fig.get_dpi()
+    fig.set_size_inches(1600.0/float(DPI),1200.0/float(DPI))
+
+    ax = Axes3D(fig)
+    ax.set_xlim3d(0, 50) # Set min and max of x axis
+    ax.set_ylim3d(0, 50) # Set min and max of y axis
+    ax.set_zlim3d(0, 1) # Set min and max of z axis
+    ax.plot_surface(xs, ys, array, rstride=1, cstride=1, cmap='GnBu', linewidth=0, antialiased=True)
+
+    # Save or show graphic
+    if filename:
+        plt.savefig(filename) # Save the figure
+    else:
+        plt.show() # Show the figure
+    plt.close(fig) # Close the figure
