b8bf3b715480da9315fc5e9fea79abc7374f140a
Sebastian Mischke <SebastianMischke@gmx.de>
2017-01-25 21:42:00 +0100

    Add all if statements

--- Smoothing/Smoothing/Program.cs
+++ Smoothing/Smoothing/Program.cs
@@ -32,6 +32,7 @@ namespace Smoothing
         double[] smooth(double[] array, int width)
         {
             double[] result = (double[])array.Clone();
+            int height = array.Length / width;
 
             //def smooth_array():
             //for i in range(0, width*height):
@@ -40,34 +41,68 @@ namespace Smoothing
                 //# Ecken
                 //# # Oben links
                 //if i==0:
-                //newarray[i] = (1-2*p)*array[i] + p*array[i+1] + p*array[i+width]
+                if (i == 0)
+                {
+                    //newarray[i] = (1-2*p)*array[i] + p*array[i+1] + p*array[i+width]
+                }
+
                 //# # Oben rechts
                 //elif i==width-1:
-                //newarray[i] = (1-2*p)*array[i] + p*array[i-1] + p*array[i+width]
+                else if (i == width - 1)
+                {
+                    //newarray[i] = (1-2*p)*array[i] + p*array[i-1] + p*array[i+width]
+                }
+
                 //# # Unten links
                 //elif i==width*height-width:
-                //newarray[i] = (1-2*p)*array[i] + p*array[i+1] + p*array[i-width]
+                else if (i == width * height - width)
+                {
+                    //newarray[i] = (1-2*p)*array[i] + p*array[i+1] + p*array[i-width]
+                }
+
                 //# # Unten rechts
                 //elif i==width*height-1:
-                //newarray[i] = (1-2*p)*array[i] + p*array[i-1] + p*array[i-width]
+                else if (i == width * height - 1)
+                {
+                    //newarray[i] = (1-2*p)*array[i] + p*array[i-1] + p*array[i-width]
+                }
 
                 //# Ränder
                 //# # Oben
                 //elif i<width:
-                //newarray[i] = (1-3*p)*array[i] + p*array[i-1] + p*array[i+1] + p*array[i+width]
+                else if (i < width)
+                {
+                    //newarray[i] = (1-3*p)*array[i] + p*array[i-1] + p*array[i+1] + p*array[i+width]
+                }
+
                 //# # Unten
                 //elif i>width*height-width:
-                //newarray[i] = (1-3*p)*array[i] + p*array[i-1] + p*array[i+1] + p*array[i-width]
+                else if (i > width * height - width)
+                {
+                    //newarray[i] = (1-3*p)*array[i] + p*array[i-1] + p*array[i+1] + p*array[i-width]
+                }
+
                 //# # Links
                 //elif i%width==0:
-                //newarray[i] = (1-3*p)*array[i] + p*array[i+1] + p*array[i-width] + p*array[i+width]
+                else if (i % width == 0)
+                {
+                    //newarray[i] = (1-3*p)*array[i] + p*array[i+1] + p*array[i-width] + p*array[i+width]
+                }
+
                 //# # Rechts
                 //elif i%width==width-1:
-                //newarray[i] = (1-3*p)*array[i] + p*array[i-1] + p*array[i-width] + p*array[i+width]
+                else if (i % width == width - 1)
+                {
+                    //newarray[i] = (1-3*p)*array[i] + p*array[i-1] + p*array[i-width] + p*array[i+width]
+                }
 
                 //# Mitte
                 //else:
-                //newarray[i = (1-4*p)*array[i] + p*array[i-1] + p*array[i+1] + p*array[i-width] + p*array[i+width]
+                else
+                {
+                    //newarray[i = (1-4*p)*array[i] + p*array[i-1] + p*array[i+1] + p*array[i-width] + p*array[i+width]
+                }
+
             }
 
             return result;
